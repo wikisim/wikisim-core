fca1624277602319567d115dc7a361cfb05061f5
Alexander James Phillips <ajp.global@gmail.com>
2025-07-24 17:12:45 +0100

    Add accessor function to get version of DataComponentOrNew

--- src/data/accessor.ts
+++ src/data/accessor.ts
@@ -9,3 +9,10 @@ export function get_id_str_of_data_component(data_component: DataComponent | New
     }
     return data_component.temporary_id.to_str()
 }
+
+
+export function get_version_of_data_component(data_component: DataComponent | NewDataComponent): number
+{
+    if (is_data_component(data_component)) return data_component.id.version
+    return 0
+}
