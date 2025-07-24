4b079ca22325dfbe24f0cc8b7633cfcc9c2870fb
Alexander James Phillips <ajp.global@gmail.com>
2025-07-24 15:20:01 +0100

    Add get_id_str_of_data_component utility function

---
+++ src/data/accessor.ts
@@ -0,0 +1,11 @@
+import { DataComponent, NewDataComponent, is_data_component } from "./interface"
+
+
+export function get_id_str_of_data_component(data_component: DataComponent | NewDataComponent): string
+{
+    if (is_data_component(data_component))
+    {
+        return data_component.id.to_str_without_version()
+    }
+    return data_component.temporary_id.to_str()
+}
