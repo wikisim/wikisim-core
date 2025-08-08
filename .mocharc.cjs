75506dbea7c14774c1e7a0be83c256bd933042d1
Alexander James Phillips <ajp.global@gmail.com>
2025-08-08 22:54:31 +0100

    Swap to mocharc.js to enable filtering to one file

---
+++ .mocharc.cjs
@@ -0,0 +1,18 @@
+
+// Copied from electrovir: https://github.com/mochajs/mocha/issues/4100#issuecomment-1166629353
+
+function is_test_file(arg)
+{
+    return arg.match(/\.[jt]sx?$/)
+}
+// const allTestFiles = './src/**/?(*.)+(spec|test).[jt]s?(x)';
+const spec = "src/**/*.test.ts"  // all test files
+
+module.exports = {
+    ignore: "src/**/*.browser.test.ts",
+    import: "tsx",
+    extensions: ["ts", "tsx"],
+    require: ["src/test/setup.js"],
+    timeout: 0,
+    ...(process.argv.slice(2).some(is_test_file) ? {} : {spec}),
+};
