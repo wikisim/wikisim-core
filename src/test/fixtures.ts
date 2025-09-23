7c9b458449564cd4a74cf7cdd6f2116ebd9dc4b2
Alexander James Phillips <ajp.global@gmail.com>
2025-09-23 12:08:43 +0100

    Add DataComponent test fixture that returns all fields populated

--- src/test/fixtures.ts
+++ src/test/fixtures.ts
@@ -1,7 +1,72 @@
-import { IdAndVersion } from "../data/id"
+import { IdAndMaybeVersion, IdAndVersion } from "../data/id"
+import { DataComponent, NewDataComponent } from "../data/interface"
+import { init_data_component, init_new_data_component } from "../data/modify"
 
 
-export function tiptap_mention_chip(args: {title: string, id: IdAndVersion }): string
+export function tiptap_mention_chip(args: {title: string, id: IdAndMaybeVersion }): string
 {
-    return `<span class="mention-chip" data-type="customMention" data-id="${args.id.id}v${args.id.version}" data-label="${args.title}">@${args.title}</span>`
+    return `<span class="mention-chip" data-type="customMention" data-id="${args.id.to_str()}" data-label="${args.title}">@${args.title}</span>`
+}
+
+
+export function new_data_component_all_fields_set(): NewDataComponent
+{
+    return init_new_data_component({
+        // This field should not and will never be changed once a component is
+        // created, but whilst a new component is being edited for the first
+        // time it may be changed.
+        owner_id: "owner_2",
+
+        editor_id: "editor_1",
+        created_at: new Date("2025-07-30T00:00:00Z"),
+        comment: "Another comment",
+        bytes_changed: 100,
+        version_type: "minor",
+        version_rolled_back_to: -10,
+
+        title: "<p>Modified Title</p>",
+        description: "<p>Modified Description</p>",
+        label_ids: [-2, -3],
+
+        input_value: "Modified Value",
+        result_value: "Modified Value",
+        recursive_dependency_ids: [ new IdAndVersion(-5, 1), new IdAndVersion(-6, 2) ],
+        value_type: "number",
+        value_number_display_type: "scientific",
+        value_number_sig_figs: 3,
+        datetime_range_start: new Date("2025-07-30T00:00:00Z"),
+        datetime_range_end: new Date("2025-07-30T00:00:00Z"),
+        datetime_repeat_every: "day",
+        units: "some units",
+        dimension_ids: [ new IdAndVersion(-1, 1) ],
+        function_arguments: [
+            {
+                id: 0,
+                name: "arg1",
+                default_value: "123",
+            }
+        ],
+        scenarios: [
+            {
+                id: 0,
+                values: { arg1: { value: "456" } },
+            }
+        ],
+
+        plain_title: "Modified Title",
+        plain_description: "Modified Description",
+
+        test_run_id: "test_run_-789",
+    })
+}
+
+
+export function data_component_all_fields_set(): DataComponent
+{
+    const {
+        temporary_id: _,
+        ...rest_new_data_component
+    } = new_data_component_all_fields_set()
+
+    return init_data_component(rest_new_data_component)
 }
