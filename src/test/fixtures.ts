02d7becacde9b82ea8bce2d49dcd630e9ba58e6c
Alexander James Phillips <ajp.global@gmail.com>
2025-10-01 19:00:08 +0100

    Fix fixture

--- src/test/fixtures.ts
+++ src/test/fixtures.ts
@@ -5,8 +5,10 @@ import { init_data_component, init_new_data_component } from "../data/modify.ts"
 
 export function tiptap_mention_chip(args: { title: string, id: IdAndMaybeVersion | string } | string, tag: "span" | "a" = "a"): string
 {
-    const id_str = typeof args === "string" ? parse_id(args).to_str()
-        : typeof args.id === "string" ? parse_id(args.id) : args.id.to_str()
+    const id_str: string = (
+        typeof args === "string" ? parse_id(args)
+        : typeof args.id === "string" ? parse_id(args.id)
+        : args.id).to_str()
     const title = typeof args === "string" ? `Some title for ${args}` : args.title
 
     if (tag === "a")
