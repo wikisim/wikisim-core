04387e2af93989e8ed97ea19a3bcae6a79b7813b
Alexander James Phillips <ajp.global@gmail.com>
2025-10-03 12:41:33 +0100

    Add constant MAX_INTERACTABLE_MEGA_BYTES

--- src/supabase/constants.ts
+++ src/supabase/constants.ts
@@ -2,5 +2,6 @@
 
 export const supabase_url = "https://sfkgqscbwofiphfxhnxg.supabase.co"
 export const supabase_anon_key = "sb_publishable_XWsGRSpmju8qjodw4gIU8A_O_mHUR1H"
+export const MAX_INTERACTABLE_MEGA_BYTES = 50
 // TODO: use this
 export const MAX_TITLE_LENGTH = 200
