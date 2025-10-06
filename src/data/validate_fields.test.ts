ddad7f5ca06d961b4d959e2d1cc66b192d7f3270
Alexander James Phillips <ajp.global@gmail.com>
2025-10-06 16:49:27 +0100

    Add validate_fields_given_value_type

--- src/data/validate_fields.test.ts
+++ src/data/validate_fields.test.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { expect } from "chai"
 import { z } from "zod"
 
+import { data_component_all_fields_set, new_data_component_all_fields_set } from "../test/fixtures"
+import { DataComponent, NewDataComponent, VALUE_TYPES, ValueType } from "./interface"
 import { make_field_validators } from "./validate_fields"
 
 
@@ -146,6 +149,183 @@ describe("validate_scenarios_from_json", () =>
         {
             expect(() => validate_scenarios_from_json(test_case))
                 .to.throw(/code": "invalid_type"/)
+        })
+    })
+})
+
+
+describe("validate_fields_given_value_type", () =>
+{
+    const { validate_fields_given_value_type } = make_field_validators(z)
+
+    it("should remove specific (optional) fields give different value_types", () =>
+    {
+        // Whether a field should be kept or removed given a specific value_type
+        // true or undefined means the field should be kept, false means it
+        // should be removed
+        const fields_keep_or_remove: {[k in keyof (DataComponent & NewDataComponent)]: true | {[v in ValueType]?: true}} = {
+            "temporary_id": true,
+            "id": true,
+
+            "owner_id": true,
+
+            "editor_id": true,
+            "created_at": true,
+            "comment": true,
+            "bytes_changed": true,
+            "version_type": true,
+            "version_rolled_back_to": true,
+
+            "title": true,
+            "description": true,
+            "label_ids": true,
+
+            "input_value": {
+                number: true,
+                // datetime_range: false,
+                number_array: true,
+                function: true,
+                // interactable: false, // interactable should only have result_value
+            },
+            "result_value": {
+                number: true,
+                // datetime_range: false,
+                number_array: true,
+                function: true,
+                interactable: true,
+            },
+            "recursive_dependency_ids": {
+                number: true,
+                // datetime_range: false,
+                number_array: true,
+                function: true,
+                // interactable: false, // for now interactable should not store this
+            },
+            "value_type": true,
+            "value_number_display_type": {
+                number: true,
+                // datetime_range: false,
+                number_array: true,
+                // function: false,
+                // interactable: false,
+            },
+            "value_number_sig_figs": {
+                number: true,
+                // datetime_range: false,
+                number_array: true,
+                // function: false,
+                // interactable: false,
+            },
+            "datetime_range_start": {
+                // number: false,
+                datetime_range: true,
+                // number_array: false,
+                // function: false,
+                // interactable: false,
+            },
+            "datetime_range_end": {
+                // number: false,
+                datetime_range: true,
+                // number_array: false,
+                // function: false,
+                // interactable: false,
+            },
+            "datetime_repeat_every": {
+                // number: false,
+                datetime_range: true,
+                // number_array: false,
+                // function: false,
+                // interactable: false,
+            },
+            "units": {
+                number: true,
+                // datetime_range: false,
+                number_array: true,
+                function: true,
+                // interactable: false,
+            },
+            "dimension_ids": {
+                // number: false,
+                // datetime_range: false,
+                number_array: true,
+                // function: false,
+                // interactable: false,
+            },
+            "function_arguments": {
+                // number: false,
+                // datetime_range: false,
+                // number_array: false,
+                function: true,
+                // interactable: false,
+            },
+            "scenarios": {
+                // number: false,
+                // datetime_range: false,
+                // number_array: false,
+                function: true,
+                // interactable: false,
+            },
+
+            "plain_title": true,
+            "plain_description": true,
+
+            "test_run_id": true,
+        }
+
+        VALUE_TYPES.forEach(value_type =>
+        {
+            const data_component = data_component_all_fields_set({ value_type })
+            const result = validate_fields_given_value_type(data_component)
+
+            Object.entries(fields_keep_or_remove).forEach(([field_, keep_or_remove]) =>
+            {
+                const field = field_ as keyof DataComponent
+
+                // Assert there is some field value unless it's temporary_id
+                // @ts-ignore
+                const is_not_temporary_id = field !== "temporary_id"
+                expect(!!data_component[field]).equals(is_not_temporary_id, `Test setup error: field "${field}" has ${is_not_temporary_id ? "no value" : "some value"}`)
+
+                const should_keep: boolean = keep_or_remove === true
+                    ? true
+                    : keep_or_remove[value_type] ?? false
+
+                if (should_keep)
+                {
+                    expect(result[field]).equals(data_component[field], `Expected to keep value for field "${field}" with value_type "${value_type}"`)
+                }
+                else
+                {
+                    expect(result[field]).equals(undefined, `Expected to remove value for field "${field}" with value_type "${value_type}"`)
+                }
+            })
+
+            // Check the id and temporary_id fields when is a new data component
+            const new_data_component = new_data_component_all_fields_set({ value_type })
+            const new_result = validate_fields_given_value_type(new_data_component)
+
+            Object.entries(fields_keep_or_remove).forEach(([field_, keep_or_remove]) =>
+            {
+                const field = field_ as keyof NewDataComponent
+
+                // Assert there is some field value unless it's id
+                // @ts-ignore
+                const is_not_id = field !== "id"
+                expect(!!new_data_component[field]).equals(is_not_id, `Test setup error: field "${field}" has ${is_not_id ? "no value" : "some value"}`)
+
+                const should_keep: boolean = keep_or_remove === true
+                    ? true
+                    : keep_or_remove[value_type] ?? false
+
+                if (should_keep)
+                {
+                    expect(new_result[field]).equals(new_data_component[field], `Expected to keep value for field "${field}" with value_type "${value_type}"`)
+                }
+                else
+                {
+                    expect(new_result[field]).equals(undefined, `Expected to remove value for field "${field}" with value_type "${value_type}"`)
+                }
+            })
         })
     })
 })
--- src/data/validate_fields.ts
+++ src/data/validate_fields.ts
@@ -4,9 +4,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { valid_value_type } from "./field_values_with_defaults.ts"
 import {
+    DataComponent,
     DBFunctionArgument,
-    DBScenario
+    DBScenario,
+    NewDataComponent,
+    ValueType
 } from "./interface.ts"
 
 
@@ -36,14 +40,96 @@ export function make_field_validators(z: any) //typeof import("zod"))
             expectation_met: z.boolean().optional(),
         })
 
+
+        const id_schema = z.object({
+            id: z.any(),
+            temporary_id: z.undefined(),
+        })
+        const temporary_id_schema = z.object({
+            id: z.undefined(),
+            temporary_id: z.any(),
+        })
+        type IdOrTempId = {
+            id: any;
+            temporary_id: undefined;
+        } | {
+            id: undefined;
+            temporary_id: any;
+        }
+
+        const ids_schema = id_schema.or(temporary_id_schema).refine(
+            (data: IdOrTempId) => {
+                const hasId = data.id !== undefined
+                const hasTempId = data.temporary_id !== undefined
+                return hasId !== hasTempId // XOR: exactly one must be true
+            },
+            { message: "Must have either id or temporary_id, but not both" }
+        )
+
+        const base_schema = ids_schema.and(z.object({
+            owner_id: z.any(),
+            editor_id: z.any(),
+            created_at: z.any(),
+            comment: z.any(),
+            bytes_changed: z.any(),
+            version_type: z.any(),
+            version_rolled_back_to: z.any(),
+            title: z.any(),
+            description: z.any(),
+            label_ids: z.any(),
+            value_type: z.string().optional(),
+            plain_title: z.any(),
+            plain_description: z.any(),
+            test_run_id: z.any(),
+        }))
+
+        // Extend for each value_type
+        const schemas_by_value_type: Record<ValueType, any> = {
+            number: base_schema.and(z.object({
+                input_value: z.any(),
+                result_value: z.any(),
+                recursive_dependency_ids: z.any(),
+                value_number_display_type: z.any(),
+                value_number_sig_figs: z.any(),
+                units: z.any(),
+            })),
+            datetime_range: base_schema.and(z.object({
+                datetime_range_start: z.any(),
+                datetime_range_end: z.any(),
+                datetime_repeat_every: z.any(),
+            })),
+            number_array: base_schema.and(z.object({
+                input_value: z.any(),
+                result_value: z.any(),
+                recursive_dependency_ids: z.any(),
+                value_number_display_type: z.any(),
+                value_number_sig_figs: z.any(),
+                units: z.any(),
+                dimension_ids: z.any(),
+            })),
+            function: base_schema.and(z.object({
+                input_value: z.any(),
+                result_value: z.any(),
+                recursive_dependency_ids: z.any(),
+                units: z.any(),
+                function_arguments: z.any(),
+                scenarios: z.any(),
+            })),
+            interactable: base_schema.and(z.object({
+                result_value: z.any(),
+            })),
+        }
+
+
         return {
             DBFunctionArgumentSchema,
             DBScenarioSchema,
+            schemas_by_value_type,
         }
     }
 
 
-    const { DBFunctionArgumentSchema, DBScenarioSchema } = zod_schemas()
+    const { DBFunctionArgumentSchema, DBScenarioSchema, schemas_by_value_type } = zod_schemas()
 
 
     function validate_function_arguments_from_json(value: unknown): DBFunctionArgument[] | undefined
@@ -62,9 +148,18 @@ export function make_field_validators(z: any) //typeof import("zod"))
         return parsed.data || undefined
     }
 
+    function validate_fields_given_value_type<V extends (DataComponent | NewDataComponent)>(data_component: V): V
+    {
+        const value_type = valid_value_type(data_component.value_type)
+        const schema = schemas_by_value_type[value_type]
+        // Use .parse to strip unknown fields
+        return schema.parse(data_component) as V
+    }
+
     return {
         validate_function_arguments_from_json,
         validate_scenarios_from_json,
+        validate_fields_given_value_type,
     }
 }
 
