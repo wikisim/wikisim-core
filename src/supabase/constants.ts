d5c4b98a7fa64a56e9397a1d53546f02ba576b51
Alexander James Phillips <ajp.global@gmail.com>
2025-10-08 11:19:50 +0100

    Add constants for interactable files

--- src/supabase/constants.ts
+++ src/supabase/constants.ts
@@ -2,6 +2,12 @@
 
 export const supabase_url = "https://sfkgqscbwofiphfxhnxg.supabase.co"
 export const supabase_anon_key = "sb_publishable_XWsGRSpmju8qjodw4gIU8A_O_mHUR1H"
-export const MAX_INTERACTABLE_MEGA_BYTES = 50
+export const INTERACTABLES_FILES_BUCKET = "interactables_files"
+export const PUBLIC_INTERACTABLES_FILES_BUCKET = "public_interactables_files"
+const MAX_INTERACTABLE_MEGA_BYTES = 50
+export const MAX_INTERACTABLE_SIZE = {
+    MEGA_BYTES: MAX_INTERACTABLE_MEGA_BYTES,
+    BYTES: MAX_INTERACTABLE_MEGA_BYTES * 1024 * 1024,
+}
 // TODO: use this
 export const MAX_TITLE_LENGTH = 200
