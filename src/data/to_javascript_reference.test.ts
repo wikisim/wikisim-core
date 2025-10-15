043cf5d17895f466d57f247845cf7940ad3ef8c9
Alexander James Phillips <ajp.global@gmail.com>
2025-10-15 16:11:23 +0100

    Add back to_javascript_reference

--- src/data/to_javascript_reference.test.ts
+++ src/data/to_javascript_reference.test.ts
@@ -1,46 +1,41 @@
-// Commenting out, not deleted, because I think we might go back to using this
-// in the future as it aids in debugging scripts by including their
-// human-readable titles
+import { expect } from "chai"
+
+import { IdAndVersion } from "./id"
+import { init_data_component } from "./modify"
+import { to_javascript_reference } from "./to_javascript_reference"
 
 
-// import { expect } from "chai"
+describe("to_javascript_reference", function ()
+{
+    it("should return a javascript safe id", function ()
+    {
+        const component = init_data_component({
+            id: new IdAndVersion(101, 2),
+            title: `  My Component ,./<>?;'\\:"|[]{}!@£#$%^&*()-=_+\`~ with_spaces`,
+        })
 
-// import { IdAndVersion } from "./id"
-// import { init_data_component } from "./modify"
-// import { to_javascript_reference } from "./to_javascript_reference"
-
-
-// describe("to_javascript_reference", function ()
-// {
-//     it("should return a javascript safe id", function ()
-//     {
-//         const component = init_data_component({
-//             id: new IdAndVersion(101, 2),
-//             title: `  My Component ,./<>?;'\\:"|[]{}!@£#$%^&*()-=_+\`~ with_spaces`,
-//         })
-
-//         expect(to_javascript_reference(component)).equals("d101v2_my_component_with_spaces")
-//     })
+        expect(to_javascript_reference(component)).equals("My_Component_with_spaces")
+    })
 
 
-//     it("should limit the title to 30 characters", function ()
-//     {
-//         const component = init_data_component({
-//             id: new IdAndVersion(101, 2),
-//             title: `  My Component ,./<>?;'\\:"|[]{}!@£#$%^&*()-=_+\`~ with_spaces25272931`,
-//         })
+    it("should limit the title to 30 characters", function ()
+    {
+        const component = init_data_component({
+            id: new IdAndVersion(101, 2),
+            title: `  My Component ,./<>?;'\\:"|[]{}!@£#$%^&*()-=_+\`~ with_spaces25272931`,
+        })
 
-//         expect(to_javascript_reference(component)).equals("d101v2_my_component_with_spaces252729")
-//     })
+        expect(to_javascript_reference(component)).equals("My_Component_with_spaces252729")
+    })
 
 
-//     it("should ignore effectively empty titles", function ()
-//     {
-//         const component = init_data_component({
-//             id: new IdAndVersion(101, 2),
-//             title: `  ,./<>   `,
-//         })
+    it("should ignore effectively empty titles", function ()
+    {
+        const component = init_data_component({
+            id: new IdAndVersion(101, 2),
+            title: `  ,./<>   `,
+        })
 
-//         expect(to_javascript_reference(component)).equals("d101v2")
-//     })
-// })
+        expect(to_javascript_reference(component)).equals("d101v2")
+    })
+})
--- src/data/to_javascript_reference.ts
+++ src/data/to_javascript_reference.ts
@@ -1,20 +1,13 @@
-// Commenting out, not deleted, because I think we might go back to using this
-// in the future as it aids in debugging scripts by including their
-// human-readable titles
+import { DataComponent } from "./interface.ts"
 
 
-// import { DataComponent } from "./interface.ts"
+export function to_javascript_reference(component: DataComponent): string
+{
+    let safe_title = component.title//.toLowerCase()
+        .replace(/[^a-z0-9_]+/gi, "_")  // Replace non-alphanumeric characters with underscores
+        .replace(/^_+/g, "")  // Trim leading underscores
+        .replace(/(_)_+/g, "$1")  // Replace multiple underscores with single
+        .slice(0, 30)  // Limit to 30 characters
 
-
-// export function to_javascript_reference(component: DataComponent): string
-// {
-//     let safe_title = component.title.toLowerCase()
-//         .replace(/[^a-z0-9_]+/g, "_")
-//         .replace(/^_+/g, "")
-//         .replace(/(_)_+/g, "$1")
-//         .slice(0, 30) // Limit to 30 characters
-
-//     safe_title = safe_title ? `_${safe_title}` : ""
-
-//     return `d${component.id.to_str()}${safe_title}`
-// }
+    return safe_title || `d${component.id.to_str()}`
+}
