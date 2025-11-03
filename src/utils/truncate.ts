afa60f8cda373c0b09b240d8fb8d2b1b4994c2df
Alexander James Phillips <ajp.global@gmail.com>
2025-11-03 00:13:01 +0000

    Remove semi-colons

--- src/utils/truncate.ts
+++ src/utils/truncate.ts
@@ -2,6 +2,6 @@
 
 export function truncate(text: string, max_length: number = 100): string
 {
-    if (text.length <= max_length) return text;
-    return text.slice(0, max_length - 3) + "...";
+    if (text.length <= max_length) return text
+    return text.slice(0, max_length - 3) + "..."
 }
