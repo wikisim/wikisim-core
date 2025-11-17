e3105c7d1fa7cc3f323551bc4e24c91a38326fd0
Alexander James Phillips <ajp.global@gmail.com>
2025-11-17 09:47:02 +0000

    Add comment regarding formatting of units to replace underscores with spaces

--- src/data/format/format_data_component_value_to_string.ts
+++ src/data/format/format_data_component_value_to_string.ts
@@ -21,7 +21,11 @@ export function format_data_component_value_to_string (data_component: DataCompo
     let num_as_str = format_number_to_string(value_as_number, value_number_sig_figs, value_number_display_type)
     if (num_as_str === "") return ""
 
-    const units_sans_underscores = units ? units.trim().replace(/_/g, " ") : ""
+    // Leaving this in for now but I think it should be removed and we instead
+    // give 100% control to the user in how to format the units.  Nothing
+    // magical.  Leaving in for now as there are historical data components
+    // that rely on this for prettier formatting.
+    const units_sans_underscores = units.trim().replaceAll("_", " ")
 
     return units_sans_underscores ? `${num_as_str} ${units_sans_underscores}` : num_as_str
 }
