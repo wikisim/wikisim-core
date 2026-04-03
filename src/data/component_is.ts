f6fcd5a1907b1ff4f048e269d30c7d0f9de68854
Alexander James Phillips <ajp.global@gmail.com>
2026-04-04 00:35:33 +0100

    Rename component_is_an_alternative to component_is

--- src/data/component_is.ts
+++ src/data/component_is.ts
@@ -2,9 +2,17 @@ import { DataComponent, NewDataComponent } from "./interface"
 
 
 /**
- * This does not guarantee that it is valid
+ * This does not guarantee that it is valid, i.e. subject_id and or according_to_id
+ * may reference a non-existent component.
  */
 export function component_is_an_alternative(component: DataComponent | NewDataComponent | undefined | null): component is ((DataComponent | NewDataComponent) & { subject_id: number, according_to_id: number })
 {
     return component?.subject_id !== undefined && component.according_to_id !== undefined
+}
+
+
+export function component_is_number(component: DataComponent | NewDataComponent | undefined | null): boolean
+{
+    if (!component) return false
+    return component.value_type === "number" || component.value_type === undefined
 }
