b5e60e881b199f9ccd00447def2d21b51869c706
Alexander James Phillips <ajp.global@gmail.com>
2026-04-07 12:05:23 +0100

    Make graph more specific by using versioned ids

--- src/data/utils/graph.test.ts
+++ src/data/utils/graph.test.ts
@@ -3,11 +3,11 @@ import * as z from "zod"
 
 import { tiptap_mention_chip } from "../../rich_text/tiptap_mention_chip"
 import { flatten_data_component_to_json, hydrate_data_component_from_json } from "../convert_between_json"
-import { IdAndVersion } from "../id"
-import { DataComponentsById } from "../interface"
+import { IdAndVersion, parse_id } from "../id"
+import { DataComponentsByIdv } from "../interface"
 import { init_data_component } from "../modify"
 import { make_field_validators } from "../validate_fields"
-import { data_components_by_id } from "./data_components_by_id"
+import { data_components_by_idv } from "./data_components_by_id"
 import { Graph, make_graph } from "./graph"
 
 
@@ -21,65 +21,52 @@ describe("graph", () =>
         init_data_component({ id: IdAndVersion.from_str("3v1"), title: "An author" }),
         init_data_component({ id: IdAndVersion.from_str("4v1"), title: "An alternative value", subject_id: 1, according_to_id: 3 }),
         init_data_component({ id: IdAndVersion.from_str("5v1"), title: "An alternative plan", input_value: `${tiptap_mention_chip("4v1")} + 3`, subject_id: 2, according_to_id: 3 }),
+
+        init_data_component({ id: IdAndVersion.from_str("1v2"), title: "A value (newer version)" }),
+        init_data_component({ id: IdAndVersion.from_str("2v2"), title: "A top plan (newer version)", input_value: `${tiptap_mention_chip("1v2")} + 2` }),
     ].map(flatten_data_component_to_json)
     const field_validators = make_field_validators(z)
     const components = data.map(d => hydrate_data_component_from_json(d, field_validators))
-    const data_map: DataComponentsById = data_components_by_id(components)
+    const data_map: DataComponentsByIdv = data_components_by_idv(components)
 
-    it("makes a graph from a list of components and id_of_interest and ignores data from other graph", () =>
+    it("makes a graph from a list of components and idv_of_interest and ignores data from other graph", () =>
     {
-        const graph = make_graph(parser, data_map, { id_of_interest: 2 })
+        const graph = make_graph(parser, data_map, { idv_of_interest: parse_id("2v2", true) })
         const minimised = minimise_graph(graph)
 
         expect(minimised.map_concept_ids).deep.equals({
-            1: 1,
-            2: 2,
+            1: "1v2",
+            2: "2v2",
         })
 
         expect(minimised.nodes).deep.equals({
-            "1": {
-                title: "A value",
+            "1v2": {
+                title: "A value (newer version)",
                 children: [],
             },
-            "1v1": {
-                title: "A value",
-                children: [],
-            },
-            "2": {
-                title: "A top plan",
-                children: ["1v1"],
-            },
-            "2v1": {
-                title: "A top plan",
-                children: ["1v1"],
+            "2v2": {
+                title: "A top plan (newer version)",
+                children: ["1v2"],
             },
         })
     })
 
 
-    it("makes a graph from a id_of_interest and includes its subject_ids referencing the id_of_concepts", () =>
+    it("makes a graph from a idv_of_interest and map_concept_ids its subject_ids (the id_of_concepts) back to the id versions of interest", () =>
     {
-        const graph = make_graph(parser, data_map, { id_of_interest: 5, id_of_concepts: 2 })
+        const graph = make_graph(parser, data_map, { idv_of_interest: parse_id("5v1", true), idv_of_concepts: parse_id("2v2", true) })
 
         const minimised = minimise_graph(graph)
 
         expect(minimised.map_concept_ids).deep.equals({
-            1: 4,
-            2: 5,
+            1: "4v1",
+            2: "5v1",
         })
 
         expect(minimised.nodes).deep.equals({
-            "4": {
-                title: "An alternative value",
-                children: [],
-            },
             "4v1": {
                 title: "An alternative value",
                 children: [],
-            },
-            "5": {
-                title: "An alternative plan",
-                children: ["4v1"],
             },
             "5v1": {
                 title: "An alternative plan",
@@ -89,67 +76,195 @@ describe("graph", () =>
     })
 
 
-    it("makes a graph from a id_of_interest and includes its subject_ids referencing the id_of_concepts", () =>
+    it("makes a graph from a idv_of_interest, uses it also for concepts and compares to another graph", () =>
     {
-        const graph = make_graph(parser, data_map, { id_of_interest: 2, id_of_comparison: 5 })
+        const graph = make_graph(parser, data_map, { idv_of_interest: parse_id("2v2", true), idv_of_comparison: parse_id("5v1", true) })
 
         const minimised = minimise_graph(graph)
 
         expect(minimised.map_concept_ids).deep.equals({
-            1: 1,
-            2: 2,
+            1: "1v2",
+            2: "2v2",
         })
 
         expect(minimised.nodes).deep.equals({
-            "1": {
-                title: "A value",
+            "1v2": {
+                title: "A value (newer version)",
                 children: [],
-                alternatives: [4],
+                alternatives: ["4v1"],
             },
-            "1v1": {
-                title: "A value",
-                children: [],
-                alternatives: [4],
-            },
-            "2": {
-                title: "A top plan",
-                children: ["1v1"],
-                alternatives: [5],
-            },
-            "2v1": {
-                title: "A top plan",
-                children: ["1v1"],
-                alternatives: [5],
+            "2v2": {
+                title: "A top plan (newer version)",
+                children: ["1v2"],
+                alternatives: ["5v1"],
             },
 
             // referenced alternatives
-            "4": {
+            "4v1": {
                 title: "An alternative value",
                 children: [],
             },
-            "5": {
+            "5v1": {
                 title: "An alternative plan",
                 children: ["4v1"],
             },
         })
     })
+
+
+    it("makes a graph from a idv_of_interest, uses a different graph for concepts and compares to another graph", () =>
+    {
+        const graph = make_graph(parser, data_map, { idv_of_concepts: parse_id("2v2", true), idv_of_interest: parse_id("5v1", true), idv_of_comparison: parse_id("2v2", true) })
+
+        const minimised = minimise_graph(graph)
+
+        expect(minimised.map_concept_ids).deep.equals({
+            1: "4v1",
+            2: "5v1",
+        })
+
+        expect(minimised.nodes).deep.equals({
+            "1v2": {
+                title: "A value (newer version)",
+                children: [],
+            },
+            "2v2": {
+                title: "A top plan (newer version)",
+                children: ["1v2"],
+            },
+
+            // referenced alternatives
+            "4v1": {
+                title: "An alternative value",
+                children: [],
+                alternatives: ["1v2"],
+            },
+            "5v1": {
+                title: "An alternative plan",
+                children: ["4v1"],
+                alternatives: ["2v2"],
+            },
+        })
+    })
+
+
+    describe("older idv_of_interest versions", () =>
+    {
+
+        it("can make a graph from an older idv_of_interest version", () =>
+        {
+            const graph = make_graph(parser, data_map, { idv_of_interest: parse_id("2v1", true) })
+
+            const minimised = minimise_graph(graph)
+
+            expect(minimised.map_concept_ids).deep.equals({
+                1: "1v1",
+                2: "2v1",
+            })
+
+            expect(minimised.nodes).deep.equals({
+                "1v1": {
+                    title: "A value",
+                    children: [],
+                },
+                "2v1": {
+                    title: "A top plan",
+                    children: ["1v1"],
+                },
+            })
+        })
+
+
+        it("can make compare a graph of an older idv_of_interest version", () =>
+        {
+            const graph = make_graph(parser, data_map, { idv_of_interest: parse_id("2v1", true), idv_of_comparison: parse_id("5v1", true) })
+
+            const minimised = minimise_graph(graph)
+
+            expect(minimised.map_concept_ids).deep.equals({
+                1: "1v1",
+                2: "2v1",
+            })
+
+            expect(minimised.nodes).deep.equals({
+                "1v1": {
+                    title: "A value",
+                    children: [],
+                    alternatives: ["4v1"],
+                },
+                "2v1": {
+                    title: "A top plan",
+                    children: ["1v1"],
+                    alternatives: ["5v1"],
+                },
+                "4v1": {
+                    title: "An alternative value",
+                    children: [],
+                },
+                "5v1": {
+                    title: "An alternative plan",
+                    children: ["4v1"],
+                },
+            })
+        })
+
+
+        it("can use an older idv_of_interest version for a comparison graph", () =>
+        {
+            const graph = make_graph(parser, data_map, { idv_of_concepts: parse_id("2v2", true), idv_of_interest: parse_id("5v1", true), idv_of_comparison: parse_id("2v1", true) })
+
+            const minimised = minimise_graph(graph)
+
+            expect(minimised.map_concept_ids).deep.equals({
+                1: "4v1",
+                2: "5v1",
+            })
+
+            expect(minimised.nodes).deep.equals({
+                "1v1": {
+                    title: "A value",
+                    children: [],
+                },
+                "2v1": {
+                    title: "A top plan",
+                    children: ["1v1"],
+                },
+                "4v1": {
+                    title: "An alternative value",
+                    children: [],
+                    alternatives: ["1v1"],
+                },
+                "5v1": {
+                    title: "An alternative plan",
+                    children: ["4v1"],
+                    alternatives: ["2v1"],
+                },
+            })
+        })
+    })
 })
 
 
+interface MinimisedNode
+{
+    title: string
+    children: string[]
+    alternatives?: string[]
+}
 function minimise_graph(graph: Graph)
 {
-    const minimised: Record<string, { title: string, children: string[], alternatives?: number[] }> = {}
+    const minimised: Record<string, MinimisedNode> = {}
     for (const [id, node] of Object.entries(graph.nodes))
     {
         minimised[id] = {
             title: node.component.title,
             children: node.children.map(c => c.to_str()),
         }
-        if (node.alternatives) minimised[id].alternatives = node.alternatives
+        if (node.alternatives) minimised[id].alternatives = node.alternatives.map(a => a.to_str())
     }
 
     return {
-        map_concept_ids: graph.map_concept_id_to_id_of_interest,
+        map_concept_ids: graph.map_concept_ido_to_idv_of_interest,
         nodes: minimised,
     }
 }
--- src/data/utils/graph.ts
+++ src/data/utils/graph.ts
@@ -1,76 +1,74 @@
 import { GenericDOMParser } from "../../rich_text/generic_interface"
 import { shared_get_referenced_ids_from_tiptap } from "../../rich_text/shared_get_referenced_ids_from_tiptap"
-import { IdAndMaybeVersion, IdAndVersion } from "../id"
-import { DataComponent, DataComponentsById } from "../interface"
+import { IdAndMaybeVersion, IdAndVersion, parse_id } from "../id"
+import { DataComponent, DataComponentsByIdv } from "../interface"
 
 
 /**
  * These may all be the same or all be different.
  *
- * @property id_of_concepts is the entry point to the knowledge graph, from which all
+ * @property idv_of_concepts is the entry point to the knowledge graph, from which all
  * the concepts can be reached.  Typically this is the top level component, the
  * narrative, the summary of the graph, or the most general perspective.
  * For example like: [https://wikisim.org/wiki/1239](National energy budget for UK)
- * Defaults to id_of_interest if not provided.
+ * Defaults to idv_of_interest if not provided.
  *
- * @property id_of_interest is the entry point to the knowledge graph containing
+ * @property idv_of_interest is the entry point to the knowledge graph containing
  * the data of interest, e.g. [https://wikisim.org/wiki/1239](National energy budget for UK according to Sustainable Energy Without the Hot Air, 2009, Prof. Mackay)
  * Required
  *
- * @property id_of_comparison is the entry point to the knowledge graph containing
+ * @property idv_of_comparison is the entry point to the knowledge graph containing
  * the data to compare to.  For example this might in the future be "National energy budget for UK according to XYZ political party)
- * Defaults to id_of_concepts if not provided, or id_of_interest if id_of_concepts is not provided.
+ * Defaults to idv_of_concepts if not provided, or idv_of_interest if idv_of_concepts is not provided.
  *
  * TODO convert these ids back into strings to allow for versioning, i.e. allow
  * for "123v10" instead of just 123.
  */
 export interface KnowledgeGraphIds
 {
-    id_of_interest: number
-    id_of_concepts?: number
+    idv_of_interest: IdAndVersion
+    idv_of_concepts?: IdAndVersion
     // TODO, change this to a list
-    id_of_comparison?: number
+    idv_of_comparison?: IdAndVersion
 }
 
 export interface Graph
 {
-    top_level_id: number
-    map_concept_id_to_id_of_interest: Record<number, number>
-    nodes: { [id: string]: GraphNode }
+    apex_id: IdAndVersion
+    map_concept_ido_to_idv_of_interest: Record<number, string>
+    nodes: { [idv: string]: GraphNode }
 }
 
 export interface GraphNode
 {
     component: DataComponent
     children: IdAndVersion[]
-    alternatives?: number[]
+    alternatives?: IdAndVersion[]
 }
 
 
-export function make_graph(parser: GenericDOMParser, data_map: DataComponentsById, ids: KnowledgeGraphIds): Graph
+export function make_graph(parser: GenericDOMParser, data_map: DataComponentsByIdv, ids: KnowledgeGraphIds): Graph
 {
-    const { id_of_interest, id_of_concepts = id_of_interest, id_of_comparison } = ids
+    const { idv_of_interest, idv_of_concepts = idv_of_interest, idv_of_comparison } = ids
 
-    const concept_graph = make_graph_inner(parser, data_map, id_of_concepts)
+    const concept_graph = make_graph_inner(parser, data_map, idv_of_concepts)
 
-    const graph_of_interest = (id_of_interest === id_of_concepts)
+    const graph_of_interest = (idv_of_interest.to_str() === idv_of_concepts.to_str())
         ? concept_graph
-        : make_graph_inner(parser, data_map, id_of_interest)
+        : make_graph_inner(parser, data_map, idv_of_interest)
 
     mutate_graph_of_interest_with_mapping_from_concept_to_node_ids({ graph_of_interest, concept_graph })
 
-    if (!id_of_comparison)
+    if (!idv_of_comparison)
     {
         return graph_of_interest
     }
 
-    console.log(`id_of_comparison: ${id_of_comparison}`)
-
-    const comparison_graph = (id_of_comparison === id_of_concepts)
+    const comparison_graph = (idv_of_comparison.to_str() === idv_of_concepts.to_str())
         ? concept_graph
-        : (id_of_comparison === id_of_interest)
+        : (idv_of_comparison.to_str() === idv_of_interest.to_str())
             ? graph_of_interest
-            : make_graph_inner(parser, data_map, id_of_comparison)
+            : make_graph_inner(parser, data_map, idv_of_comparison)
 
     mutate_graph_of_interest_with_alternatives({ graph_of_interest, comparison_graph })
 
@@ -78,14 +76,14 @@ export function make_graph(parser: GenericDOMParser, data_map: DataComponentsByI
 }
 
 
-function make_graph_inner(parser: GenericDOMParser, data_map: DataComponentsById, top_level_id: number): Graph
+function make_graph_inner(parser: GenericDOMParser, data_map: DataComponentsByIdv, apex_id: IdAndVersion): Graph
 {
-    const top_level_component = data_map[top_level_id]
-    if (!top_level_component) throw new Error(`Top level component with id ${top_level_id} not found`)
+    const apex_component = data_map[apex_id.to_str()]
+    if (!apex_component) throw new Error(`Apex (top level) component with id ${apex_id.to_str()} not found`)
 
     const graph: Graph = {
-        map_concept_id_to_id_of_interest: {},
-        top_level_id,
+        map_concept_ido_to_idv_of_interest: {},
+        apex_id,
         nodes: {},
     }
 
@@ -95,19 +93,18 @@ function make_graph_inner(parser: GenericDOMParser, data_map: DataComponentsById
 
         const direct_dependency_ids = shared_get_referenced_ids_from_tiptap(parser, component.input_value || "")
         const children = direct_dependency_ids
-            .map(id => data_map[id.to_str()])
-            .filter((c): c is DataComponent => !!c)
+            .map(id =>
+            {
+                const c = data_map[id.to_str()]
+                if (!c) console.log(`Component with id ${id.to_str()} not found in data_map while processing component with id ${component.id.to_str()}`)
+                // if (!c) throw new Error(`Component with id ${id.to_str()} not found in data_map while processing component with id ${component.id.to_str()}`)
+                return c
+            })
+            .filter((c): c is DataComponent =>
+            {
+                return !!c
+            })
             .map(make_graph_recursive)
-
-        if (graph.nodes[component.id.id])
-        {
-            throw new Error(`Multiple versions of component with id ${component.id.id} found in data. Please ensure only the latest version is included. Found versions: ${graph.nodes[component.id.id]!.component.id.to_str()} and ${component.id.to_str()}`)
-        }
-
-        graph.nodes[component.id.id] = {
-            component,
-            children,
-        }
 
         graph.nodes[component.id.to_str()] = {
             component,
@@ -117,7 +114,7 @@ function make_graph_inner(parser: GenericDOMParser, data_map: DataComponentsById
         return component.id
     }
 
-    make_graph_recursive(top_level_component)
+    make_graph_recursive(apex_component)
 
     make_ids_unique(graph)
 
@@ -129,12 +126,12 @@ function mutate_graph_of_interest_with_mapping_from_concept_to_node_ids(args: { 
 {
     const { graph_of_interest, concept_graph } = args
 
-    const map_concept_id_to_node_id: Record<number, number> = {}
+    const map_concept_id_to_node_id: Record<string, string> = {}
 
     // First load up the default mapping from concept ids to themselves
     Object.values(concept_graph.nodes).forEach(node =>
     {
-        map_concept_id_to_node_id[node.component.id.id] = node.component.id.id
+        map_concept_id_to_node_id[node.component.id.id] = node.component.id.to_str()
     })
 
     // Now go through graph_of_interest and if any nodes have a subject_id,
@@ -143,16 +140,22 @@ function mutate_graph_of_interest_with_mapping_from_concept_to_node_ids(args: { 
     {
         const subject_id = node.component.subject_id
         if (!subject_id) return
-        map_concept_id_to_node_id[subject_id] = node.component.id.id
+        map_concept_id_to_node_id[subject_id] = node.component.id.to_str()
     })
 
-    graph_of_interest.map_concept_id_to_id_of_interest = map_concept_id_to_node_id
+    graph_of_interest.map_concept_ido_to_idv_of_interest = map_concept_id_to_node_id
 }
 
 
 function mutate_graph_of_interest_with_alternatives(args: { graph_of_interest: Graph, comparison_graph: Graph }): undefined
 {
     const { graph_of_interest, comparison_graph } = args
+
+    const comparison_graph_nodes_by_ido: Record<number, GraphNode> = {}
+    Object.values(comparison_graph.nodes).forEach(node =>
+    {
+        comparison_graph_nodes_by_ido[node.component.id.id] = node
+    })
 
     // Are there any nodes in graph_of_interest who reference the base through
     // component.subject_id?  If so add the nodes from the base as alternatives
@@ -162,36 +165,41 @@ function mutate_graph_of_interest_with_alternatives(args: { graph_of_interest: G
         const subject_id = node.component.subject_id
         if (!subject_id) return
 
-        const base_node = comparison_graph.nodes[subject_id]
+        const base_node = comparison_graph_nodes_by_ido[subject_id]
         if (!base_node) return
 
         node.alternatives = node.alternatives || []
-        node.alternatives.push(base_node.component.id.id)
+        node.alternatives.push(base_node.component.id)
     })
 
     // Are there any nodes in the comparison_graph which have component.subject_id
     // referencing nodes in the graph_of_interest? If so add these nodes from the
     // base as alternatives to the nodes in the graph_of_interest
-    const map_subject_id_from_base_to_base_node_id: Record<number, number> = {}
+    const map_subject_id_from_base_to_base_node_id: Record<number, IdAndVersion[]> = {}
 
     Object.values(comparison_graph.nodes).forEach(node =>
     {
         const subject_id = node.component.subject_id
         if (!subject_id) return
 
-        map_subject_id_from_base_to_base_node_id[subject_id] = node.component.id.id
+        const ids = map_subject_id_from_base_to_base_node_id[subject_id] || []
+        ids.push(node.component.id)
+        map_subject_id_from_base_to_base_node_id[subject_id] = ids
     })
 
     Object.values(graph_of_interest.nodes).forEach(node =>
     {
-        const base_node_id = map_subject_id_from_base_to_base_node_id[node.component.id.id]
-        if (!base_node_id) return
+        const base_node_ids = map_subject_id_from_base_to_base_node_id[node.component.id.id]
+        if (!base_node_ids) return
 
-        const base_node = comparison_graph.nodes[base_node_id.toString()]
-        if (!base_node) return
+        base_node_ids.forEach(base_node_id =>
+        {
+            const base_node = comparison_graph.nodes[base_node_id.to_str()]
+            if (!base_node) return
 
-        node.alternatives = node.alternatives || []
-        node.alternatives.push(base_node.component.id.id)
+            node.alternatives = node.alternatives || []
+            node.alternatives.push(base_node.component.id)
+        })
     })
 
     make_ids_unique(graph_of_interest)
@@ -205,9 +213,17 @@ function make_ids_unique(graph: Graph): void
     // Make ids unique
     Object.values(graph.nodes).forEach(node =>
     {
-        node.children = Array.from(new Set(node.children))
-        node.alternatives = node.alternatives && Array.from(new Set(node.alternatives))
+        node.children = unique_ids(node.children)
+        node.alternatives = node.alternatives && unique_ids(node.alternatives)
     })
+}
+
+
+function unique_ids(ids: IdAndVersion[]): IdAndVersion[]
+{
+    const idv_strings = ids.map(id => id.to_str())
+    const unique_idv_strings = Array.from(new Set(idv_strings))
+    return unique_idv_strings.map(id_str => parse_id(id_str, true))
 }
 
 
@@ -220,12 +236,6 @@ function ensure_all_references_ids_are_in_graph(args: { graph_of_interest: Graph
     function check_id(id: IdAndMaybeVersion | number)
     {
         const id_is_number = typeof id === "number"
-        const id_without_version = id_is_number ? id.toString() : id.to_str_without_version()
-
-        if (!graph_of_interest.nodes[id_without_version])
-        {
-            missing_ids.add(id_without_version)
-        }
 
         if (!id_is_number && id.has_version() && !graph_of_interest.nodes[id.to_str()])
         {
